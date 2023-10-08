5baa82747989d45904daedf1890f7b6e3e0eda20
Vasyl Maslov <maslovvasil@gmail.com>
2023-10-08 22:34:18 +0300

    add Home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-// import { lazy } from 'react';
+import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Appbar from './Appbar';
 
-// const Home = lazy(() => import('../views/Home'));
+const Home = lazy(() => import('../views/Home'));
 // const Movies = lazy(() => import('../views/Movies'));
 // const MovieDetails = lazy(() => import('../views/MovieDetails'));
 // const NotFoundPage = lazy(() => import('../views/NotFoundPage'));
-// const Cast = lazy(() => import('../views/Cast'));
+const Cast = lazy(() => import('../views/Cast'));
 // const Reviews = lazy(() => import('../views/Reviews'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Appbar />}>
-        {/* <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
+        <Route index element={<Home />} />
+        {/* <Route path="movies" element={<Movies />} />
 
         <Route path="movies/:movieId/" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />

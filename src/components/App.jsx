b8fdf99d9d54552e290eaa94f8998b743158166b
Vasyl Maslov <maslovvasil@gmail.com>
2023-10-09 22:26:54 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Appbar from './Appbar';
 
 const Home = lazy(() => import('../views/Home'));
-// const Movies = lazy(() => import('../views/Movies'));
-// const MovieDetails = lazy(() => import('../views/MovieDetails'));
+const Movies = lazy(() => import('../views/Movies'));
+const MovieDetails = lazy(() => import('../views/MovieDetails'));
 // const NotFoundPage = lazy(() => import('../views/NotFoundPage'));
 const Cast = lazy(() => import('../views/Cast'));
 // const Reviews = lazy(() => import('../views/Reviews'));
@@ -14,14 +14,14 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Appbar />}>
         <Route index element={<Home />} />
-        {/* <Route path="movies" element={<Movies />} />
+        <Route path="movies" element={<Movies />} />
 
         <Route path="movies/:movieId/" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+          {/* <Route path="reviews" element={<Reviews />} /> */}
         </Route>
 
-        <Route path="*" element={<NotFoundPage />} /> */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Route>
     </Routes>
   );
